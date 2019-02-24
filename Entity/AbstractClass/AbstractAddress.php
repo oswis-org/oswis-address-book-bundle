b5c85802e1529eaa5224c97f2ee76ee8cc538757
zakjakub <mail@jakubzak.eu>
2019-02-24 17:45:18 +0100

    Some changes.

--- Entity/AbstractClass/AbstractAddress.php
+++ Entity/AbstractClass/AbstractAddress.php
@@ -3,7 +3,6 @@
 namespace Zakjakub\OswisAddressBookBundle\Entity\AbstractClass;
 
 use Zakjakub\OswisCoreBundle\Traits\Entity\AddressTrait;
-use Zakjakub\OswisCoreBundle\Traits\Entity\BasicEntityTrait;
 use Zakjakub\OswisCoreBundle\Traits\Entity\NameableBasicTrait;
 
 abstract class AbstractAddress
