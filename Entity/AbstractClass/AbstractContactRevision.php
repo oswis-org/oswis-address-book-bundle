f0fcad172830d984f093b046cca2254fd135c67f
zakjakub <mail@jakubzak.eu>
2019-02-24 23:50:07 +0100

    Some changes.

--- Entity/AbstractClass/AbstractContactRevision.php
+++ Entity/AbstractClass/AbstractContactRevision.php
@@ -6,16 +6,6 @@ use Zakjakub\OswisAddressBookBundle\Entity\ContactImage;
 use Zakjakub\OswisCoreBundle\Entity\AbstractRevision;
 use Zakjakub\OswisCoreBundle\Traits\Entity\BasicEntityTrait;
 
-/**
- * @Doctrine\ORM\Mapping\Entity()
- * @Doctrine\ORM\Mapping\Table(name="address_book_abstract_contact_revision")
- * @Doctrine\ORM\Mapping\InheritanceType("JOINED")
- * @Doctrine\ORM\Mapping\DiscriminatorColumn(name="discriminator", type="text")
- * @Doctrine\ORM\Mapping\DiscriminatorMap({
- *   "address_book_person_revision" = "PersonRevision",
- *   "address_book_organization_revision" = "OrganizationRevision"
- * })
- */
 abstract class AbstractContactRevision extends AbstractRevision
 {
 
