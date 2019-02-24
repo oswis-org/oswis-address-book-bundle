faed5fe4dfa7a56ea0b3e31a29a0a7eed8564041
zakjakub <mail@jakubzak.eu>
2019-02-24 20:58:08 +0100

    Some changes.

--- ZakjakubOswisAddressBookBundle.php
+++ ZakjakubOswisAddressBookBundle.php
@@ -2,9 +2,13 @@
 
 namespace Zakjakub\OswisAddressBookBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use ZakJakub\OswisAddressBookBundle\DependencyInjection\ZakjakubOswisAddressBookExtension;
 
 class ZakjakubOswisAddressBookBundle extends Bundle
 {
-
+    final public function getContainerExtension(): Extension
+    {
+        return new ZakjakubOswisAddressBookExtension();
+    }
 }
