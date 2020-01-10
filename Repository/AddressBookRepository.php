68cb244bfb2a1a3da4a5976c3bccf232845dcb97
zakjakub <mail@jakubzak.eu>
2020-01-11 00:22:18 +0100

    Some changes and updates.

--- Repository/AddressBookRepository.php
+++ Repository/AddressBookRepository.php
@@ -7,9 +7,14 @@
 namespace Zakjakub\OswisAddressBookBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Zakjakub\OswisAddressBookBundle\Entity\AddressBook\AddressBook;
 
 class AddressBookRepository extends EntityRepository
 {
+    public function findOneBy(array $criteria, array $orderBy = null): ?AddressBook
+    {
+        $addressBook = parent::findOneBy($criteria, $orderBy);
 
-
+        return $addressBook instanceof AddressBook ? $addressBook : null;
+    }
 }
--- Repository/PersonRepository.php
+++ Repository/PersonRepository.php
@@ -11,10 +11,4 @@ use Zakjakub\OswisAddressBookBundle\Entity\AddressBook\AddressBook;
 
 class PersonRepository extends EntityRepository
 {
-    public function findOneBy(array $criteria, array $orderBy = null): ?AddressBook
-    {
-        $addressBook = parent::findOneBy($criteria, $orderBy);
-
-        return $addressBook instanceof AddressBook ? $addressBook : null;
-    }
 }
