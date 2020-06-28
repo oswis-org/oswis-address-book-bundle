00c19e1c11ac153a89d6e8da839dec1bae5ff5f9
zakjakub <mail@jakubzak.eu>
2020-06-28 18:34:34 +0200

    Some changes, updates and fixes.

--- Entity/ContactDetailType.php
+++ Entity/ContactDetailType.php
@@ -3,205 +3,134 @@
  * @noinspection MethodShouldBeFinalInspection
  */
 
-namespace OswisOrg\OswisAddressBookBundle\Entity;
+namespace OswisOrg\OswisAddressBookBundle\Form;
 
-use InvalidArgumentException;
-use OswisOrg\OswisCoreBundle\Entity\NonPersistent\Nameable;
-use OswisOrg\OswisCoreBundle\Interfaces\Common\NameableInterface;
-use OswisOrg\OswisCoreBundle\Traits\Common\NameableTrait;
-use OswisOrg\OswisCoreBundle\Traits\Common\TypeTrait;
+use OswisOrg\OswisAddressBookBundle\Entity\ContactDetail;
+use OswisOrg\OswisCoreBundle\Exceptions\OswisException;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\Exception\AccessException;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Exception\ConstraintDefinitionException;
+use Symfony\Component\Validator\Exception\InvalidOptionsException;
+use Symfony\Component\Validator\Exception\MissingOptionsException;
 
-/**
- * @Doctrine\ORM\Mapping\Entity(repositoryClass="OswisOrg\OswisAddressBookBundle\Repository\ContactDetailTypeRepository")
- * @Doctrine\ORM\Mapping\Table(name="address_book_contact_detail_type")
- * @ApiPlatform\Core\Annotation\ApiResource(
- *   attributes={
- *     "filters"={"search"},
- *     "access_control"="is_granted('ROLE_MANAGER')"
- *   },
- *   collectionOperations={
- *     "get"={
- *       "access_control"="is_granted('ROLE_MANAGER')",
- *       "normalization_context"={"groups"={"address_book_contact_detail_types_get"}},
- *     },
- *     "post"={
- *       "access_control"="is_granted('ROLE_MANAGER')",
- *       "denormalization_context"={"groups"={"address_book_contact_detail_types_post"}}
- *     }
- *   },
- *   itemOperations={
- *     "get"={
- *       "access_control"="is_granted('ROLE_MANAGER')",
- *       "normalization_context"={"groups"={"address_book_contact_detail_type_get"}},
- *     },
- *     "put"={
- *       "access_control"="is_granted('ROLE_MANAGER')",
- *       "denormalization_context"={"groups"={"address_book_contact_detail_type_put"}}
- *     },
- *     "delete"={
- *       "access_control"="is_granted('ROLE_ADMIN')",
- *       "denormalization_context"={"groups"={"address_book_contact_detail_type_delete"}}
- *     }
- *   }
- * )
- * @ApiPlatform\Core\Annotation\ApiFilter(ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter::class)
- * @OswisOrg\OswisCoreBundle\Filter\SearchAnnotation({
- *     "id",
- *     "appUser.username",
- *     "appUser.description",
- *     "appUser.note"
- * })
- * @Doctrine\ORM\Mapping\Cache(usage="NONSTRICT_READ_WRITE", region="address_book_contact")
- */
-class ContactDetailType implements NameableInterface
+class ContactDetailType extends AbstractType
 {
-    public const TYPE_EMAIL = 'email';
-    public const TYPE_URL = 'url';
-    public const TYPE_PHONE = 'telephone';
-    public const TYPE_SOCIAL = 'social';
-    public const TYPE_MESSENGER = 'messenger';
-    public const TYPE_VOIP = 'voip';
-
-    public const ALLOWED_TYPES = [
-        self::TYPE_URL       => ['name' => 'URL'],
-        self::TYPE_EMAIL     => ['name' => 'E-mail'],
-        self::TYPE_PHONE     => ['name' => 'Telefon'],
-        self::TYPE_SOCIAL    => ['name' => 'Profil na sociální síti'],
-        self::TYPE_MESSENGER => ['name' => 'Internetový komunikátor'],
-        self::TYPE_VOIP      => ['name' => 'Internetová telefonie'],
+    protected const PATTERNS = [
+        \OswisOrg\OswisAddressBookBundle\Entity\ContactDetailType::TYPE_URL => "^(\+420|\+421)? ?[1-9][0-9]{2} ?[0-9]{3} ?[0-9]{3}$",
     ];
 
-    use NameableTrait;
-    use TypeTrait;
+    protected const TYPES = [
+        \OswisOrg\OswisAddressBookBundle\Entity\ContactDetailType::TYPE_EMAIL => EmailType::class,
+        \OswisOrg\OswisAddressBookBundle\Entity\ContactDetailType::TYPE_PHONE => TelType::class,
+        \OswisOrg\OswisAddressBookBundle\Entity\ContactDetailType::TYPE_URL   => UrlType::class,
+    ];
 
     /**
-     * @var string|null $contactSchema Schema of type of contact
-     * @Doctrine\ORM\Mapping\Column(type="string", nullable=true)
+     * @param FormBuilderInterface $builder
+     * @param array                $options
      */
-    protected ?string $contactSchema = null;
-
-    /**
-     * Show in address book preview?
-     * @Doctrine\ORM\Mapping\Column(type="boolean", nullable=true)
-     */
-    protected ?bool $showInPreview = null;
-
-    /**
-     * @Doctrine\ORM\Mapping\Column(type="string", nullable=true)
-     */
-    protected ?string $formLabel = null;
-
-    /**
-     * @Doctrine\ORM\Mapping\Column(type="string", nullable=true)
-     */
-    protected ?string $formHelp = null;
-
-    /**
-     * ContactDetailType constructor.
-     *
-     * @param Nameable|null $nameable
-     * @param string|null   $schema
-     * @param bool|null     $showInPreview
-     * @param string|null   $type
-     * @param string|null   $formLabel
-     * @param string|null   $formHelp
-     *
-     * @throws InvalidArgumentException
-     */
-    public function __construct(
-        ?Nameable $nameable = null,
-        ?string $schema = null,
-        ?bool $showInPreview = null,
-        ?string $type = null,
-        ?string $formLabel = null,
-        ?string $formHelp = null
-    ) {
-        $this->setFieldsFromNameable($nameable);
-        $this->setContactSchema($schema);
-        $this->setShowInPreview($showInPreview);
-        $this->setType($type);
-        $this->setFormLabel($formLabel);
-        $this->setFormHelp($formHelp);
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add(
+            'content',
+            TextType::class,
+            array(
+                'label'    => false,
+                'required' => $options['content_required'],
+                'attr'     => ['placeholder' => 'Kontakt'],
+            )
+        );
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            static function (FormEvent $event) use ($options) {
+                $contactDetail = $event->getData();
+                assert($contactDetail instanceof ContactDetail);
+                $detailType = $contactDetail->getDetailType();
+                $detailTypeType = $contactDetail->getDetailType() ? $contactDetail->getDetailType()->getType() : null;
+                $form = $event->getForm();
+                $options = array(
+                    'label'       => $detailType ? $detailType->getFormLabel() : false,
+                    'required'    => $options['content_required'],
+                    'attr'        => [/*'autocomplete' => $contactDetail->getContactType() ? $contactDetail->getContactType()->getType() : true*/],
+                    'help'        => $detailType ? $detailType->getFormHelp() : null,
+                    'constraints' => self::getConstraintsByType($detailTypeType),
+                );
+                $pattern = self::getPatternByType($detailTypeType);
+                if ($pattern) {
+                    $options['attr']['pattern'] = $pattern;
+                }
+                $form->add('content', self::getTypeByType($detailTypeType), $options);
+            }
+        );
     }
 
-    public static function getAllowedTypesDefault(): array
+    /**
+     * @param string|null $type
+     *
+     * @return array
+     * @throws ConstraintDefinitionException
+     * @throws InvalidOptionsException
+     * @throws MissingOptionsException
+     */
+    public static function getConstraintsByType(?string $type = null): array
     {
-        return [
-            self::TYPE_URL,
-            self::TYPE_EMAIL,
-            self::TYPE_PHONE,
-            self::TYPE_SOCIAL,
-            self::TYPE_MESSENGER,
-            self::TYPE_VOIP,
-        ];
-    }
+        if (\OswisOrg\OswisAddressBookBundle\Entity\ContactDetailType::TYPE_EMAIL === $type) {
+            return [new Email(['mode' => 'strict', 'message' => 'Zadaná adresa {{ value }} není platná.'])];
+        }
+        if (\OswisOrg\OswisAddressBookBundle\Entity\ContactDetailType::TYPE_PHONE === $type) {
+            return [
+                new Regex(
+                    [
+                        'pattern' => "/^(\+420|\+421)? ?[1-9][0-9]{2} ?[0-9]{3} ?[0-9]{3}$/",
+                        'message' => 'Zadané číslo {{ value }} není platným českým nebo slovenským telefonním číslem.',
+                    ]
+                ),
+                new Length(['min' => 9, 'max' => 15]),
+            ];
+        }
 
-    public static function getAllowedTypesCustom(): array
-    {
         return [];
     }
 
-    public function getFormLabel(): ?string
+    public static function getPatternByType(?string $type = null): ?string
     {
-        return $this->formLabel;
+        return self::PATTERNS[$type] ?? null;
     }
 
-    public function setFormLabel(?string $formLabel): void
+    public static function getTypeByType(?string $type = null): string
     {
-        $this->formLabel = $formLabel;
-    }
-
-    public function getFormHelp(): ?string
-    {
-        return $this->formHelp;
-    }
-
-    public function setFormHelp(?string $formHelp): void
-    {
-        $this->formHelp = $formHelp;
-    }
-
-    public function getShowInPreview(): ?bool
-    {
-        return $this->showInPreview;
-    }
-
-    public function setShowInPreview(?bool $showInPreview): void
-    {
-        $this->showInPreview = $showInPreview;
+        return self::TYPES[$type] ?? TextType::class;
     }
 
     /**
-     * @param string      $value
-     * @param string|null $description
+     * @param OptionsResolver $resolver
      *
-     * @param string|null $name
-     *
-     * @return string
-     * @noinspection UnknownInspectionInspection
-     * @noinspection HtmlUnknownTag
+     * @throws AccessException
      */
-    public function getFormatted(string $value, ?string $description, ?string $name = null): string
+    public function configureOptions(OptionsResolver $resolver): void
     {
-        return strtr($this->getContactSchema(), ['$<value>' => $value, '$<description>' => $description, '$<name>' => $name]);
+        $resolver->setDefaults(
+            array(
+                'data_class'       => ContactDetail::class,
+                'content_required' => false,
+                // 'attr' => ['class' => 'col-md-6'],
+            )
+        );
     }
 
-    /**
-     * Get schema of contact detail.
-     * @return string
-     */
-    public function getContactSchema(): ?string
+    public function getName(): string
     {
-        return $this->contactSchema;
-    }
-
-    /**
-     * Set schema of contact detail.
-     *
-     * @param string|null $contactSchema
-     */
-    public function setContactSchema(?string $contactSchema): void
-    {
-        $this->contactSchema = $contactSchema;
+        return 'address_book_contact_detail';
     }
 }
